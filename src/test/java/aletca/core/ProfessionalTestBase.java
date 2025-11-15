72bd38ad69433ddd3ca7756e97ca2d9305be30e2
Aletca <aletca@gmail.com>
2025-11-15 11:12:20 +0300

    added my package

---
+++ src/test/java/aletca/core/ProfessionalTestBase.java
@@ -0,0 +1,46 @@
+package aletca.core;
+
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserContext;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+
+public class ProfessionalTestBase {
+    protected static Playwright playwright;
+    protected static Browser browser;
+    protected BrowserContext context;
+    protected Page page;
+
+    @BeforeAll
+    static void initializePlaywright() {
+        playwright = Playwright.create();
+        browser = playwright.chromium().launch();
+    }
+
+    @BeforeEach
+    void createBrowserContext() {
+        context = browser.newContext();
+        page = context.newPage();
+    }
+
+    @AfterEach
+    void closeBrowserContext() {
+        if (context != null) {
+            context.close();
+        }
+    }
+
+    @AfterAll
+    static void closePlaywright() {
+        if (browser != null) {
+            browser.close();
+        }
+        if (playwright != null) {
+            playwright.close();
+        }
+    }
+}
